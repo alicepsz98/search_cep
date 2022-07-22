dc08b67a3686dd990c094d27f14557d6508080c5
Alice Souza <alicepsouzaba1998@gmail.com>
2022-07-22 12:56:03 -0300

    created global styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import GlobalStyles from "./styles/global";
 
 const App = () => {
   return (
     <div>
       Buscador de CEP
+      <GlobalStyles />
     </div>
   );
 };
